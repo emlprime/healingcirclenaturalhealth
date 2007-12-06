a2c3ccf44d1e70c872f8d226d069f5af4a1a37c7
Peter <peter@paradox.(none)>
2007-12-06 19:19:20 +0000

    Adding basic setup files

--- urls.py
+++ urls.py
@@ -1,9 +1,15 @@
 from django.conf.urls.defaults import *
+from healingcirclenaturalhealth.settings import MEDIA_ROOT
 
-urlpatterns = patterns('',
-    # Example:
-    # (r'^healingcirclenaturalhealth/', include('healingcirclenaturalhealth.foo.urls')),
+urlpatterns = patterns('django.views.generic.simple',
+    (r'^$', 'direct_to_template', {'template': 'home.html'}),
+    (r'^help/$', 'direct_to_template', {'template': 'help.html'}),
+)
 
-    # Uncomment this for admin:
+urlpatterns += patterns('',
+    (r'^product/', include('healingcirclenaturalhealth.product.urls')),
+    (r'^accounts/logout/', 'django.contrib.auth.views.logout'),
+    (r'^accounts/login/', 'django.contrib.auth.views.login'),
+    (r'^media/(.*)$', 'django.views.static.serve', {'document_root': MEDIA_ROOT}),
 #     (r'^admin/', include('django.contrib.admin.urls')),
 )

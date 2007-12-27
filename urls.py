c6450af319f8526db474821a1e293bb4ffa77f0d
Peter <peter@paradox.(none)>
2007-12-27 16:50:00 +0000

    added the resume and intake sections

--- urls.py
+++ urls.py
@@ -4,13 +4,11 @@ from healingcirclenaturalhealth.settings import MEDIA_ROOT
 urlpatterns = patterns('django.views.generic.simple',
     (r'^$', 'direct_to_template', {'template': 'home.html'}),
     (r'^services/$', 'direct_to_template', {'template': 'services.html'}),
+    (r'^resume/$', 'direct_to_template', {'template': 'resume.html'}),
+    (r'^intake/$', 'direct_to_template', {'template': 'intake.html'}),
     (r'^help/$', 'direct_to_template', {'template': 'help.html'}),
 )
 
 urlpatterns += patterns('',
-    (r'^product/', include('healingcirclenaturalhealth.product.urls')),
-    (r'^accounts/logout/', 'django.contrib.auth.views.logout'),
-    (r'^accounts/login/', 'django.contrib.auth.views.login'),
     (r'^media/(.*)$', 'django.views.static.serve', {'document_root': MEDIA_ROOT}),
-#     (r'^admin/', include('django.contrib.admin.urls')),
 )

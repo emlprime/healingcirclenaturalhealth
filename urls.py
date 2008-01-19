10dfac101b6781c2c37a4a9c1814c7e52991bbcc
Peter <peter@paradox.(none)>
2008-01-19 21:10:20 +0000

    misc changes and image additions

--- urls.py
+++ urls.py
@@ -4,6 +4,7 @@ from healingcirclenaturalhealth.settings import MEDIA_ROOT
 urlpatterns = patterns('django.views.generic.simple',
     (r'^$', 'direct_to_template', {'template': 'home.html'}),
     (r'^services/$', 'direct_to_template', {'template': 'services.html'}),
+    (r'^consultation/$', 'direct_to_template', {'template': 'consultation.html'}),
     (r'^resume/$', 'direct_to_template', {'template': 'resume.html'}),
     (r'^intake/$', 'direct_to_template', {'template': 'intake.html'}),
     (r'^help/$', 'direct_to_template', {'template': 'help.html'}),

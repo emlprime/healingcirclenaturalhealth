5e79ef88f4681a40a9f8708f72d2274ed1ca562a
Laura <laura@emlprime.com>
2009-02-18 14:05:18 -0800

    added instructional dvd page

--- urls.py
+++ urls.py
@@ -6,6 +6,7 @@ urlpatterns = patterns('django.views.generic.simple',
     (r'^services/$', 'direct_to_template', {'template': 'services.html'}),
     (r'^consultation/$', 'direct_to_template', {'template': 'consultation.html'}),
     (r'^resume/$', 'direct_to_template', {'template': 'resume.html'}),
+    (r'^instructional_dvd/$', 'direct_to_template', {'template': 'instructional_dvd.html'}),
     (r'^intake/$', 'direct_to_template', {'template': 'intake.html'}),
     (r'^help/$', 'direct_to_template', {'template': 'help.html'}),
 )
